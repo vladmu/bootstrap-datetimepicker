f617dcf52661dfcecb91287ba1381503c6a57231
Vlad Mustafin <v.mustafin@gmail.com>
2016-06-19 17:59:46 +0300

    next and prev externally accessed

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -1416,6 +1416,9 @@
          * a clone when setting a private variable.
          *
          ********************************************************************************/
+        picker.next = actions.next;
+        picker.previous = actions.previous;
+
         picker.destroy = function () {
             ///<summary>Destroys the widget and removes all attached event listeners</summary>
             hide();
